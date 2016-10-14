f17a891493232d66f05aeb6aa531cd48b684e711
Geoff Davis <gdavis@archermalmo.com>
2016-10-14 17:31:06 -0500

    init sinatra project

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://rubygems.org'
+gem 'sinatra', :github => 'sinatra/sinatra'
+
+# deps
+gem 'haml'
---
+++ app.rb
@@ -0,0 +1,29 @@
+# app
+require 'sinatra'
+
+# settings
+set :haml, :format => :html5
+set :layout_options => { :views => 'views', :layouts => 'layouts' }
+
+# functions
+
+def layout(layout_name)
+	return "../layouts/#{layout_name}".to_sym
+end
+
+# routes
+get '/' do 
+	@meta_data = {
+		:title => 'Home',
+		:page_class => 'home'
+	}
+	@page_data = {
+		:title => 'Home'
+	}
+
+	haml :index, :layout => layout('default'), :locals => {:meta => @meta_data, :page => @page_data}
+end
+
+get '/case-studies/:study' do |study|
+	haml :cstudy, :locals => {:study => study}
+end
